83dd6cd66a1ad264917e954220e8b2b0b430ccf2
tekenscodr <ensleybdarku@gmail.com>
2022-10-30 18:17:23 +0000

    Bro Bro

--- qrcode.js
+++ qrcode.js
@@ -3,4 +3,13 @@ const User = require('./model')
 
 
 
-const  
+ // let userId = await User.findById(req.params.id)
+        //     res.json(id._id);
+        // let username = await User.findById(req.params.firstname)
+        //     res.json(firstname.firstname);
+ 
+
+QRCode.toCanvas(document.getElementById('canvas'), 'sample text', function (error) {
+    if (error) console.error(error)
+    console.log('success!');
+  })
