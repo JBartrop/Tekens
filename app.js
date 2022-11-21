11a77e8fe0504d8329e05595359d7b93e8bf40bd
tekenscodr <ensleybdarku@gmail.com>
2022-11-21 21:10:04 +0000

    yoyo

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ require('./init_mogo')
 const qrcode = require('qrcode')
 const User = require('./model');
 const { verifyAccessToken, signAccessToken } = require("./helpers/jwt_helpers");
+const { findById } = require("./model");
+const { Types } = require("mongoose");
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -22,19 +24,15 @@ app.get("/register", function(request, response){
     response.render("register")
 })
 
-app.post("/register", function(request, response){
+app.post("/register", async function (request, response){
     const newUser = new User({
         email : request.body.email,
         password : request.body.password
     })
+    const savedUser = await newUser.save()
+    const accessToken = await signAccessToken(savedUser.id)
+    response.render("signin")
 
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }else{
-            response.render("admin");
-        }
-    });
 
 });
 
@@ -47,53 +45,63 @@ app.get("/login", function(request, response){
 app.post("/login", async function(request, response){
     try {
         
-    const usermail = await request.body.email;
+    const email = await request.body.email;
     const password = await request.body.password;
-
-    const user = await User.findOne({email: usermail}, 
-        async function(err, foundUser){
-        if(err){
-            console.log(err);
-        }else{
-            if(foundUser){
-                if(foundUser.password === password){
-                    const accessToken = await signAccessToken(user.id)
-                    res.json({ accessToken }).render("qrcode")
-                }else{
-                    console.log(err);
-                }
-            }
-        }
-    });
-} catch (error) {
-   
     
-}
+    const user = await User.findOne({email: email});
+    if (!user) console.log("Wrong Email")
+
+    const isMatch = await user.isValidPassword(password)
+    if(!isMatch) {
+        console.log("Wrong password")
+    }else{
+        //const accessToken = await signAccessToken(user.id)
+       const id = await user.id 
+        console.log(id)
+        let input_text = `http://localhost:3000/${id}`;
+        await qrcode.toDataURL(input_text, (err, src) => {
+            if(err) response.send('something is wrong');
+            //response.send(input_text)
+            response.status(200).render("qrcode", {qr_code: src });
+        // response.redirect("/auth2")
+        // response.json({accessToken});
+        }
+)}
+const userlog = await request.params.id    
+const isScanned = request.params.isScanned
+// setInterval(() => {
+//         if(isScanned !== true) {
+//          response.render("admin");
+//     }else{
+//         console.log ("You fucking don't know what the heck you're doing ;)")
+//     }
+// }, 4000);
+
+} catch (error) {}
 }); 
 
-app.get("/auth2", async(req, res) => {
-    // const accessToken = await signAccessToken(user.id)
-    // let input_text = `localhost:3000/accessToken?${accessToken}`;
-    let input_text = 'https://hqstaff.newpatrioticparty.org/index.php?id=NPP-HQSTAFF-2022-0072';
-
-    // const token = await verifyAccessToken(user.id)
-    
-    // if(!token) res.send('check token properly');
-    
-    qrcode.toFile('./qrcode.png', 'https://hqstaff.newpatrioticparty.org/index.php?id=NPP-HQSTAFF-2022-0040')
-
-    // qrcode.toDataURL(input_text, (err, src) => {
-    //     if(err) res.send('something is wrong');
-    //     res.render( "qrcode", {
-    //         qr_code: src
-    //     });
-    // })
+app.get("/qrcode", async function(req, res, next){
+    res.render("qrcode")
 })
 
+app.put("/:id", async function(req, res, next) {
+   try {
+    const getID =  req.params.id
+    console.log(getID)
+    if(!getID){
+       res.send('Wrong token')
+    } else{
+        await User.findByIdAndUpdate(getID, {isScanned: true});
+       res.send("you got it")
+    }
+   } catch (error) {
+    
+   }
+   })
 
-//app.get("/admin", function(request, response){
-//    response.render("admin")
-//});
+app.get("/admin", function(request, response){
+   response.render("admin")
+});
 
 const PORT = process.env.PORT || 3000
 
